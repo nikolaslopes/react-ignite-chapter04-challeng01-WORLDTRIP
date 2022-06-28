968aeac7926e55e26aafdd1628591f8fc419f3f6
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-06-28 14:34:50 -0400

    :pencil2: created global country interface

---
+++ src/interfaces/global.ts
@@ -0,0 +1,29 @@
+export interface ICity {
+  id: number
+  name: string
+  capital: string
+  thumb: string
+  flag: string
+}
+
+export interface ICountry {
+  id: number
+  name: string
+  info: string
+  resume: string
+  main_image: string
+  secondary_image: string
+  meta: {
+    countries_amount: number
+    languages_amount: number
+    cities_amount: {
+      head: number
+      total: number
+    }
+  }
+  cities: Array<ICity>
+}
+
+export interface IContinents {
+  continents: Array<ICountry>
+}
